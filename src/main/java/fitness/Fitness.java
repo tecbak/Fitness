f120e61c5c4e59b5632f804c83dc57d0deb28632
Dmytro Rud <Dmytro_Rud@epam.com>
2016-09-28 13:48:21 +0300

    extract volume()

--- src/main/java/fitness/Fitness.java
+++ src/main/java/fitness/Fitness.java
@@ -11,32 +11,29 @@ public class Fitness {
     private List<Drink> drinks = new ArrayList<>();
     private List<Meal> meals = new ArrayList<>();
 
-    /*Eat methods*/
     public void eat(int kcal, LocalDateTime dt) {
         Meal meal = new Meal(kcal, dt);
         meals.add(meal);
     }
 
-    public int eaten() {
-        int sum = 0;
-        for (Meal meal : meals) {
-            sum += meal.volume();
-        }
-        return sum;
-    }
-
-    /*Drink methods*/
     public void drink(int ml, LocalDateTime dt) {
         Drink drink = new Drink(ml, dt);
         drinks.add(drink);
     }
 
+    public int eaten() {
+        return volume(meals);
+    }
+
     public int drunk() {
+        return volume(drinks);
+    }
+
+    private int volume(List<? extends Consumable> consumables) {
         int sum = 0;
-        for (Drink drink : drinks) {
-            sum += drink.volume();
+        for (Consumable consumable : consumables) {
+            sum += consumable.volume();
         }
         return sum;
     }
-
 }
